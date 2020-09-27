a199a3c5e106b5d68340291775b277fbbc8a10db
henrystoldt <hhstoldt@ucalgary.ca>
2020-09-26 22:32:37 -0600

    runTests fixed on Linux

--- test/runTests.py
+++ test/runTests.py
@@ -7,44 +7,49 @@ def runTests(level=3):
         Level 2 Excludes tests in test_SimulationRunners
         Level 3 Runs all unittest test cases
     '''
-    suite = unittest.TestSuite()
-
     # Make sure we're running from the main MAPLEAF directory
     cwd = os.getcwd()
     if os.path.basename(cwd) == "test":
         os.chdir("..")
 
+    # Make sure main directory is in path
     cwd = os.getcwd()
-    if cwd not in sys.path:
-        sys.path.insert(0, cwd)
+    if cwd in sys.path:
+        sys.path.remove(cwd)
+    sys.path.insert(0, cwd)
 
     #### Find tests ####
+    suite = unittest.TestSuite()
+
     if level < 3:
-        items = [ "test/" + x for x in os.listdir("test") ]
+        items = [ ("test/" + x) for x in os.listdir("test") ]
 
         for item in items:
-            moduleName = item.replace("test/", "test.")
+            moduleName = item.replace("test/", "")
             moduleName = moduleName.replace(".py", "")
             
-            # Exclude functional, long-running tests from the SimulationRunners folder/package
+            # Exclude functional, long-running tests from the SimulationRunners and Rocket folders/packages
             if level == 1 and os.path.isdir(item) and "SimulationRunners" not in item and "Rocket" not in item and "test_" in item:
                 print(moduleName)
                 suite.addTests(unittest.defaultTestLoader.discover(moduleName))
 
-            if level == 2 and os.path.isdir(item) and "SimulationRunners" not in item and "test_" in item:
+            # Only exclude SimulationRunners
+            elif level == 2 and os.path.isdir(item) and "SimulationRunners" not in item and "test_" in item:
                 print(moduleName)
                 suite.addTests(unittest.defaultTestLoader.discover(moduleName))
             
-            # Add test modules from the test/ 
-            if os.path.isfile(item) and "test_" in item:
+            # Always add test modules from test/ 
+            elif os.path.isfile(item) and "test_" in item:
                 print(moduleName)
                 suite.addTests(unittest.defaultTestLoader.loadTestsFromName(moduleName))
+    
     elif level == 3:
-        suite.addTests(unittest.defaultTestLoader.discover(start_dir="."))
+        # Run all tests
+        suite.addTests(unittest.defaultTestLoader.discover(start_dir="test"))
 
     #### Run Tests ####
     runner = unittest.TextTestRunner(verbosity=3)
-    runner.run(suite)
+    result = runner.run(suite)
 
 if __name__ == "__main__":
-    runTests()
+    runTests(1)
