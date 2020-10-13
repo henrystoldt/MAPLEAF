583a72675c0a8cf1ddd8f5e577fb3b09e93c1579
[Sergio] <[sergio.olaciregui@ucalgary.ca]>
2020-10-12 22:41:33 -0600

    TVC Propulsion Edits
    Work in Progress: Determining Thrust Magnitude Directions

--- MAPLEAF/Rocket/Propulsion.py
+++ MAPLEAF/Rocket/Propulsion.py
@@ -155,10 +155,11 @@ class TabulatedMotor(RocketComponent, SubDictReader):
             # Copied the implementation from fins.py and changed 'numfins' to numTVC', 'finList' to 'TVCList', 'finAngle' to 'TVCAngle'. Need to define what these are somewhere else most likely?
             for i in range(self.numTVC): # numTVC should always be equal to 2 for thrust vectoring
                 self.TVCList[i].TVCAngle = self.TVCList[i].getDeflection(time)
+                
         
         # Create Vector
         #TODO: Define xAngle, yAngle, zAngle, probably come from the acxtuatorList. Need to verify the form the values will be returned in (x = [0], y=[1], z=[2]?)
-        thrust = Vector(thrustMagnitude*xAngle, thrustMagnitude*yAngle, thrustMagnitude*zAngle) # Scale these by the thrust vectoring angles. x = cosasinbx, y = cosacosby, z = sinc, need to get these.
+        thrust = Vector(thrustMagnitude*TVCList[0].TVCAngle, thrustMagnitude*TVCList[1].TVCAngle, thrustMagnitude*) # Scale these by the thrust vectoring angles. x = cosasinbx, y = cosacosby, z = sinc, need to get these.
         
         # Log and return
         self.rocket.appendToForceLogLine(" {:>10.4f}".format(thrust.Z))
