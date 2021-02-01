674883967dd59bd43882ced82c6888fda3e0654c
henrystoldt <henrystoldt@users.noreply.github.com>
2021-02-01 11:23:07 -0700

    --nCores deprecated, switch to --parallel, improvements to cost function evaluation in Optimization

--- MAPLEAF/IO/Logging.py
+++ MAPLEAF/IO/Logging.py
@@ -113,8 +113,9 @@ def getSystemInfo(printToConsole=False):
 
     try:
         # Add current git status
-        currentCommit = check_output(['git', 'rev-parse', 'HEAD']).decode()[:-1]
-        currentBranch = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode()[:-1]
+        currentDirectory = os.path.dirname(__file__)
+        currentCommit = check_output(['git', 'rev-parse', 'HEAD'], cwd=currentDirectory).decode()[:-1]
+        currentBranch = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=currentDirectory).decode()[:-1]
         result.append("# MAPLEAF, branch: {}, latest commit: {}".format(currentBranch, currentCommit))
     except:
         result.append("# ERROR: Could not obtain current branch/commit info from git. Ensure command-line version of git is installed: https://git-scm.com/downloads")
