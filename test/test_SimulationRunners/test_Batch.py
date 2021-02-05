4802480f85a1933d77d4171531c701aa1453bd73
henrystoldt <henrystoldt@users.noreply.github.com>
2021-02-04 19:59:00 -0700

    Batch simulation test case bugfix

--- test/test_SimulationRunners/test_Batch.py
+++ test/test_SimulationRunners/test_Batch.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from MAPLEAF.IO import SimDefinition
 from MAPLEAF.Main import isBatchSim
-from MAPLEAF.SimulationRunners.Batch import _checkResult, BatchRun, _validate
+from MAPLEAF.SimulationRunners.Batch import _checkResult, BatchRun, _validate, CaseResult
 from test.testUtilities import captureOutput
 
 
@@ -22,14 +22,15 @@ class TestBatchSim(unittest.TestCase):
         self.assertFalse(isBatchSim(normalDefinition))
 
     def test_checkResult(self):
-        origPassedTests = self.batchRun.nTestsOk
-        origFailedTests = self.batchRun.nTestsFailed
+        caseResult = CaseResult('fakeCase', 0, 0, 0, [], [])
+        origPassedTests = caseResult.testsPassed
+        origFailedTests = caseResult.testsFailed
 
         # Should pass
         with captureOutput() as (out, err):
-            _checkResult(self.batchRun, "fakeCase", "FakeColumn", 1.125, 1.125)
+            _checkResult(self.batchRun, caseResult, "fakeCase", "FakeColumn", 1.125, 1.125)
 
-        self.assertEqual(self.batchRun.nTestsOk, origPassedTests+1)
+        self.assertEqual(caseResult.testsPassed, origPassedTests+1)
         output = out.getvalue().strip()
         self.assertTrue(" ok " in output)
         self.assertTrue(" FAIL " not in output)
@@ -37,9 +38,9 @@ class TestBatchSim(unittest.TestCase):
         
         # Should fail
         with captureOutput() as (out, err):
-            _checkResult(self.batchRun, "fakeCase", "FakeColumn", 1.1, 1.0)
+            _checkResult(self.batchRun, caseResult, "fakeCase", "FakeColumn", 1.1, 1.0)
 
-        self.assertEqual(self.batchRun.nTestsFailed, origFailedTests+1)
+        self.assertEqual(caseResult.testsFailed, origFailedTests+1)
         output = out.getvalue().strip()
         self.assertTrue(" ok " not in output)
         self.assertTrue(" FAIL " in output)
