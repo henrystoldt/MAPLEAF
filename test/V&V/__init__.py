a419ed8e1956b345b46e4ac5f39268848496934b
henrystoldt <henrystoldt@users.noreply.github.com>
2021-02-05 09:03:49 -0700

    Put image below text on V&V web page, fixed name of two comparison data files in regression test suite

--- test/V&V/__init__.py
+++ test/V&V/__init__.py
@@ -1,9 +1,9 @@
 '''
-<img src="https://raw.githubusercontent.com/henrystoldt/MAPLEAF/master/Resources/testingMeme.jpg"/>  
-
 This is MAPLEAF's verification and validation page.
 Results from all of the cases defined in `./MAPLEAF/Examples/Batch/regressionTests.mapleaf` are shown in the 'modules' below, one for each case.
 Content is automatically regenerated to remain up to date with the master branch.
+
+<img src="https://raw.githubusercontent.com/henrystoldt/MAPLEAF/master/Resources/testingMeme.jpg"/>  
 
 .. note:: To run this test suite yourself, run `$mapleaf-batch`
 
